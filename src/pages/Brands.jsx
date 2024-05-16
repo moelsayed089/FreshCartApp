95d889e032bd8ea3e505f3dd767711bf65aafa45
moelsayed011 <me6758478@gmail.com>
2024-05-16 17:55:38 +0300

    Handle Some UI

--- src/pages/Brands.jsx
+++ src/pages/Brands.jsx
@@ -13,10 +13,10 @@ export const Brands = () => {
   if (isLoading) return <Loading color={'#14B014'} width={"80"} />
   return <>
     <div className="mx-auto h-16 max-w-screen-xl py-8 px-4 sm:px-6 lg:px-8 ">
-      <div class="grid grid-cols-1 gap-4 md:grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 lg:gap-8">
+      <div className="grid grid-cols-1 gap-4 md:grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 lg:gap-8">
 
-        {ResData.map((item)=>{
-          return <div class="rounded-lg bg-gray-50">
+        {ResData.map((item,idx)=>{
+          return <div key={idx} className="rounded-lg bg-gray-50">
             <div className="w-100 px-3 py-5">
               <img src={item.image} className="w-full  object-cover  rounded-lg bg-slate-900" alt="" />
               <h1 className="text-center text-3xl font-medium py-3 text-green-600">{item.name}</h1>
