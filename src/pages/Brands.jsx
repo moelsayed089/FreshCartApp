afd00eabdd481924c99597a2b934091ccad19364
moelsayed011 <me6758478@gmail.com>
2024-05-16 17:33:11 +0300

    Handel Categories and Brands Companent + API

--- src/pages/Brands.jsx
+++ src/pages/Brands.jsx
@@ -1,6 +1,31 @@
+import { useQuery } from "react-query"
+import instance from "../config/axios.config"
+import { Loading } from "../components/ui/Loading"
 
 export const Brands = () => {
-  return (
-    <div>Brands</div>
-  )
+  async function GetAllBrands(){
+    return await instance.get('brands')
+  }
+
+  const { data, isLoading } = useQuery('AllBarnds', GetAllBrands)
+  const ResData = data?.data.data
+
+  if (isLoading) return <Loading color={'#14B014'} width={"80"} />
+  return <>
+    <div className="mx-auto h-16 max-w-screen-xl py-8 px-4 sm:px-6 lg:px-8 ">
+      <div class="grid grid-cols-1 gap-4 md:grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 lg:gap-8">
+
+        {ResData.map((item)=>{
+          return <div class="rounded-lg bg-gray-50">
+            <div className="w-100 px-3 py-5">
+              <img src={item.image} className="w-full  object-cover  rounded-lg bg-slate-900" alt="" />
+              <h1 className="text-center text-3xl font-medium py-3 text-green-600">{item.name}</h1>
+            </div>
+          </div>
+        })}
+
+      </div>
+  </div>
+    
+  </>
 }
--- src/pages/Categories.jsx
+++ src/pages/Categories.jsx
@@ -1,6 +1,31 @@
+import { useQuery } from "react-query"
+import { Loading } from "../components/ui/Loading"
+import instance from "../config/axios.config"
 
 export const Categories = () => {
-  return (
-    <div>Categories</div>
-  )
+  async function GetAllCategories() {
+    return await instance.get('categories')
+  }
+
+  const { data, isLoading } = useQuery('Allcategories', GetAllCategories)
+  const ResData = data?.data.data
+
+  if (isLoading) return <Loading color={'#14B014'} width={"80"} />
+  return <>
+    <div className="mx-auto h-16 max-w-screen-xl py-8 px-4 sm:px-6 lg:px-8 ">
+      <div class="grid grid-cols-1 gap-4 md:grid-cols-2 sm:grid-cols-3 lg:grid-cols-5 lg:gap-8">
+
+        {ResData.map((item) => {
+          return <div class="rounded-lg bg-gray-50">
+            <div className="w-100 px-3 py-5 flex flex-col">
+              <img src={item.image} className="w-100 h-[250px]  rounded-lg bg-slate-900" alt="" />
+              <h1 className="text-center text-3xl font-medium py-3 text-green-600">{item.name}</h1>
+            </div>
+          </div>
+        })}
+
+      </div>
+    </div>
+
+  </>
 }
